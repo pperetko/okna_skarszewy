66a14e10614c8f073b38713d8fc7e3d6b2df49a7
pperetko <pperetko@gmail.com>
2016-04-05 22:25:05 +0200

    opis okien

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Created by pperetko on 18.03.16.
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
     //$('#nav-kontakt').click(function () {
     //    $('.kontakt').show("slow", function () {
     //    });
@@ -12,33 +12,26 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('#idcancel').click(function () {
         $('.kontakt').hide();
     });
 });
 
 
-
-
-
-
-function ValidateFrom(form){
+function ValidateFrom(form) {
     var next = true;
-    if (next)
-    {
+    if (next) {
         var TestEmail = /^(.[A-Za-z0-9\-]*\w)+@+([A-Za-z0-9\-]*\w)+(\.[A-Za-z]*\w)+$/;
 
         var Wynik = form['email'].value.match(TestEmail);
-        if (Wynik == null)
-
-        {
-            document.getElementById('input-email').setAttribute('style','border-width: 4px; border-color: red');
+        if (Wynik == null) {
+            document.getElementById('input-email').setAttribute('style', 'border-width: 4px; border-color: red');
             document.getElementById('input-email').focus();
             next = false;
 
-        } else{
-            document.getElementById('input-email').setAttribute('style','border-width: 1px; border-color: black');
+        } else {
+            document.getElementById('input-email').setAttribute('style', 'border-width: 1px; border-color: black');
 
         }
 
@@ -46,12 +39,12 @@ function ValidateFrom(form){
     //Walidacja numeru telefonu
     if (next) {
         var Testphone = /^\d{3}-?\d{3}-?\d{3}$/;
-        var wynik1=form['phone'].value.match(Testphone);
+        var wynik1 = form['phone'].value.match(Testphone);
         if (wynik1 == null) {
-            document.getElementById('input-phone').setAttribute('style','border-width: 4px; border-color: red');
+            document.getElementById('input-phone').setAttribute('style', 'border-width: 4px; border-color: red');
             next = false;
         } else {
-            document.getElementById('input-phone').setAttribute('style','border-width: 1px; border-color: black');
+            document.getElementById('input-phone').setAttribute('style', 'border-width: 1px; border-color: black');
             document.getElementById('input-phone').focus();
         }
 
@@ -60,18 +53,14 @@ function ValidateFrom(form){
 }
 
 
-
-
-
-function getVar (name, url) {
-    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&#]*)')).exec(url)) {
+function getVar(name, url) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&#]*)')).exec(url)) {
         return decodeURIComponent(name[1]);
     }
 }
 
 
-
-$(function() {
+$(function () {
 
     $("form").submit(function () {
         return (ValidateFrom(this));
@@ -80,18 +69,33 @@ $(function() {
 });
 
 
-
-function infoOnclick(aparam){
-    document.getElementById('dobroplast-profil-1').setAttribute('style','display:none');
-    document.getElementById('dobroplast-profil-2').setAttribute('style','display:none');
+function infoOnclick(aparam) {
+    document.getElementById('dobroplast-profil-1').setAttribute('style', 'display:none');
+    document.getElementById('dobroplast-profil-2').setAttribute('style', 'display:none');
     document.getElementById('dobroplast-profil-1').style.display = 'none';
     document.getElementById('dobroplast-profil-2').style.display = 'none';
- if (aparam===1){
-     document.getElementById('dobroplast-profil-1').style.display = 'block';
-     document.getElementById('dobroplast-profil-2').style.display = 'block';
-     document.getElementById('dobroplast-profil-1').setAttribute('style','display:block');
-     document.getElementById('dobroplast-profil-2').setAttribute('style','display:block');
- }
+    document.getElementById('dobroplast-profil-3').setAttribute('style', 'display:none');
+    document.getElementById('dobroplast-profil-4').setAttribute('style', 'display:none');
+    document.getElementById('dobroplast-profil-3').style.display = 'none';
+    document.getElementById('dobroplast-profil-4').style.display = 'none';
+    document.getElementById('dobroplast-profil-5').setAttribute('style', 'display:none');
+    document.getElementById('dobroplast-profil-6').setAttribute('style', 'display:none');
+    document.getElementById('dobroplast-profil-5').style.display = 'none';
+    document.getElementById('dobroplast-profil-6').style.display = 'none';
+    if (aparam === 1) {
+        document.getElementById('dobroplast-profil-1').style.display = 'block';
+        document.getElementById('dobroplast-profil-2').style.display = 'block';
+        document.getElementById('dobroplast-profil-1').setAttribute('style', 'display:block');
+        document.getElementById('dobroplast-profil-2').setAttribute('style', 'display:block');
+        document.getElementById('dobroplast-profil-3').style.display = 'block';
+        document.getElementById('dobroplast-profil-4').style.display = 'block';
+        document.getElementById('dobroplast-profil-3').setAttribute('style', 'display:block');
+        document.getElementById('dobroplast-profil-4').setAttribute('style', 'display:block');
+        document.getElementById('dobroplast-profil-5').style.display = 'block';
+        document.getElementById('dobroplast-profil-6').style.display = 'block';
+        document.getElementById('dobroplast-profil-5').setAttribute('style', 'display:block');
+        document.getElementById('dobroplast-profil-6').setAttribute('style', 'display:block');
+    }
 
 }
 
