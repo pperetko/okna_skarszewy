55c81d768079beb2b8018d5076696076b5da29d5
pperetko <pperetko@gmail.com>
2016-04-09 11:58:54 +0200

    praca nad roletami

--- js/main.js
+++ js/main.js
@@ -87,11 +87,13 @@ function infoOnclick(aparam) {
     document.getElementById('admar-profil-3').setAttribute('style', 'display:none');
     document.getElementById('artus-profil-1').setAttribute('style', 'display:none');
     document.getElementById('sztukmar-profil-1').setAttribute('style', 'display:none');
+    document.getElementById('sztukmar-profil-2').setAttribute('style', 'display:none');
     document.getElementById('admar-profil-1').style.display = 'none';
     document.getElementById('admar-profil-2').style.display = 'none';
     document.getElementById('admar-profil-3').style.display = 'none';
     document.getElementById('artus-profil-1').style.display = 'none';
     document.getElementById('sztukmar-profil-1').style.display = 'none';
+    document.getElementById('sztukmar-profil-2').style.display = 'none';
 
 
     if (aparam === 1) {
@@ -115,11 +117,13 @@ function infoOnclick(aparam) {
         document.getElementById('admar-profil-3').setAttribute('style', 'display:block');
         document.getElementById('artus-profil-1').setAttribute('style', 'display:block');
         document.getElementById('sztukmar-profil-1').setAttribute('style', 'display:block');
+        document.getElementById('sztukmar-profil-2').setAttribute('style', 'display:block');
         document.getElementById('admar-profil-1').style.display = 'block';
         document.getElementById('admar-profil-2').style.display = 'block';
         document.getElementById('admar-profil-3').style.display = 'block';
         document.getElementById('artus-profil-1').style.display = 'block';
         document.getElementById('sztukmar-profil-1').style.display = 'block';
+        document.getElementById('sztukmar-profil-2').style.display = 'block';
 
     }
 }
